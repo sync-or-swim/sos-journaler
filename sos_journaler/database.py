fedde6990623142855df7ec24cdd910912af6259
ignormies <bryce.beagle@gmail.com>
2020-11-13 00:03:13 -0800

    Fix retention policy duplicate issue (#49)
    * Create a default retention policy
    
    * Don't try to create the retention policy if it already exists

--- sos_journaler/database.py
+++ sos_journaler/database.py
@@ -4,6 +4,8 @@ from sos_journaler import config
 
 
 class FIXMDatabase(InfluxDBClient):
+    DEFAULT_RETENTION_POLICY_NAME = "hard_cutoff"
+
     def __init__(self):
         super().__init__(config.influxdb_hostname, config.influxdb_port,
                          config.influxdb_username, config.influxdb_password,
@@ -15,10 +17,13 @@ class FIXMDatabase(InfluxDBClient):
         # Create the database if it doesn't already exist
         self.create_database(config.influxdb_database)
 
-        # Configure the retention policy
-        self.create_retention_policy(
-            name='hard_cutoff',
-            duration=config.influxdb_retention_duration,
-            replication="1",
-            default=True
-        )
+        # Check if any retention policies exist with our name
+        if all(policy["name"] != self.DEFAULT_RETENTION_POLICY_NAME
+               for policy in self.get_list_retention_policies()):
+            # Configure the retention policy
+            self.create_retention_policy(
+                name=self.DEFAULT_RETENTION_POLICY_NAME,
+                duration=config.influxdb_retention_duration,
+                replication="1",
+                default=True
+            )
